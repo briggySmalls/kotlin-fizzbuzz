f538b662bf9c620e6ada647d0dd4dce22ac323a5
briggySmalls <briggySmalls90@gmail.com>
2021-03-27 18:33:44 +0000

    Implement FP fizzbuzz

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,40 @@
+/**
+Helper function for checking if a value is divided exactly
+ */
+fun isDivisibleBy(value: Int, divisor: Int): Boolean {
+    return value % divisor == 0
+}
+
+/**
+ * Data class for holding a special word to print, if the test evaluates to true
+ */
+data class SpecialWord(val string: String, val test: (it: Int) -> Boolean)
+
+/**
+ * Parameterised constructor for creating fizzbuzz sequences
+ */
+fun fizzBuzzSequence(specials: List<SpecialWord>): Sequence<String> {
+    var it = 1
+    fun next(): String {
+        // Run through testing all of our special words and accumulate a string
+        var returnValue = specials.fold("") { acc, special -> if (special.test(it)) acc + special.string else acc }
+        // If we've not had any matches at all, then return the default
+        if (returnValue == "") {
+            returnValue = "$it"
+        }
+        // Increment
+        it++
+        return returnValue
+    }
+    return generateSequence(::next)
+}
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val params = listOf(
+        SpecialWord("fizz") { isDivisibleBy(it, 3) },
+        SpecialWord("buzz") { isDivisibleBy(it, 5) },
+    )
+    for (str in fizzBuzzSequence(params).take(100)) {
+        println(str)
+    }
 }
